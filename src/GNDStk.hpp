fe9c5d0f6ac5e0079be9309a3cb977235fa21410
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2019-09-23 05:14:06 +0200

    Trying to get things working.

--- src/GNDStk.hpp
+++ src/GNDStk.hpp
@@ -1,6 +1,8 @@
 #ifndef GNDSTK_HPP
 #define GNDSTK_HPP
 
+#include "knoop.hpp"
+
 namespace njoy{
   namespace gndstk {
   }
---
+++ src/GNDStk/test/GNDStk.test.cpp
@@ -0,0 +1,8 @@
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+#include "GNDStk.hpp"
+
+SCENARIO( "Testing GNDStk" ){
+  
+}
