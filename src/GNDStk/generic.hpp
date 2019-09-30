91712cb294ef9ff2872a8476ce43fa9dfa0ef78e
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2019-09-30 07:34:28 -0600

    Removing the Node class.
    It's not necessary as it is in knoop.

--- src/GNDStk/generic.hpp
+++ src/GNDStk/generic.hpp
@@ -1,5 +1,7 @@
 namespace generic{
 
-#include "GNDStk/generic/Node.hpp"
+using Node_t = knoop::Node< std::string >;
+#include "GNDStk/generic/src/node.hpp"
+#include "GNDStk/generic/src/getString.hpp"
 
 } // namespace generic
--- src/GNDStk/generic/Node/src/metadata.hpp
+++
@@ -1,7 +0,0 @@
-const std::string& metadata( std::string key ) const{
-  return this->metadata_.at( key );
-}
-
-void metadata( std::string key, std::string value ){
-  this->metadata_.emplace( key, value );
-}
