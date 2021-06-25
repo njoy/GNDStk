a83628b87d9aae83b052a0a93d7808936ed0cc1d
Martin Staley <kipnflip@gmail.com>
2021-06-24 19:37:05 -0600

    Blast it, the SFINAE again was the underlying cause of the vector remake warnings.
    finish(Node) had things in the correct order. 1. Get fields from Node into derived class (Values). 2. Get those parameters (length, start, valueType) from Values - where they were just read - into BodyText. 3. Call get() to transform the string to a vector, using the parameters (in particular valueType) that were just brought from Values to BodyText.
    
    Basically, though, pullFromDerived still wasn't working.
    
    Turns out I "double fixed" that "content" thing. I was sending content to hasLength etc. Then hasLength and other add another "content." So basically the SFINAE was checking for content.content.length. :-/ Grrr.

--- src/GNDStk/BodyText/src/detail.hpp
+++ src/GNDStk/BodyText/src/detail.hpp
@@ -44,21 +44,21 @@ using remove_cvrefs_t = typename remove_cvrefs<Ts...>::type;
 template<class T, class = int>
 struct has_length : std::false_type { };
 template<class T>
-struct has_length<T,decltype((void)T::content.length,0)>
+struct has_length<T,decltype((void)T::length,0)>
    : std::true_type { };
 
 // has_start
 template<class T, class = int>
 struct has_start : std::false_type { };
 template<class T>
-struct has_start<T,decltype((void)T::content.start,0)>
+struct has_start<T,decltype((void)T::start,0)>
    : std::true_type { };
 
 // has_valueType
 template<class T, class = int>
 struct has_valueType : std::false_type { };
 template<class T>
-struct has_valueType<T,decltype((void)T::content.valueType,0)>
+struct has_valueType<T,decltype((void)T::valueType,0)>
    : std::true_type { };
 
 // ------------------------
