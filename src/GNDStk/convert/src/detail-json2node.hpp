91da1bfcaef294d699896d2dbfc7347dfc200d20
Martin Staley <staley@lanl.gov>
2022-05-31 14:27:08 -0600

    More JSON code updates.

--- src/GNDStk/convert/src/detail-json2node.hpp
+++ src/GNDStk/convert/src/detail-json2node.hpp
@@ -33,9 +33,8 @@ inline std::string json_array(const orderedJSON &meta)
          oss << m.get<std::uint64_t>();
       else if (m.is_number_float())
          oss << m.get<double>();
-      else {
-         assert(false);
-      }
+      else
+         json2node_error("JSON array element has unexpected type");
    }
 
    return oss.str();
@@ -49,8 +48,7 @@ inline std::string json_array(const orderedJSON &meta)
 template<class NODE>
 bool json_pair(
    NODE &node, const orderedJSON &siblings,
-   const std::string &key, const orderedJSON &value,
-   const bool plain
+   const std::string &key, const orderedJSON &value, const bool metadatum
 ) {
    if (value.is_null()) {
       // *** null
@@ -72,19 +70,17 @@ bool json_pair(
       node.add(key, value.get<std::string>());
    } else if (value.is_array()) {
       // *** array
-      if (plain)
+      if (metadatum)
          node.add(key, json_array(value));
       else {
          Node &child = node.add(key);
          child.add("#pcdata").add("#text", json_array(value));
-         for (auto sib = siblings.begin();  sib != siblings.end();  ++sib) {
+         for (const auto &sib : siblings.items()) {
             if (sib.key() == key + "#nodename")
-               child.name = sib->get<std::string>();
-            else if (sib.key() == key + "#metadata") {
-               const orderedJSON &jsub = sib.value();
-               for (auto attr = jsub.begin();  attr != jsub.end();  ++attr)
-                  json_pair(child, orderedJSON{}, attr.key(), *attr, true);
-            }
+               child.name = sib.value().get<std::string>();
+            else if (sib.key() == key + "#metadata")
+               for (const auto &m : sib.value().items())
+                  json_pair(child, orderedJSON{}, m.key(), m.value(), true);
          }
       }
    } else if (value.is_object()) {
@@ -97,8 +93,8 @@ bool json_pair(
          throw;
       }
    } else {
-      // *** unknown!
-      json2node_error("Unknown/unhandled JSON value type");
+      // *** unknown
+      json2node_error("JSON value type unknown or not handled");
    }
 
    return true;
@@ -111,23 +107,23 @@ bool json_pair(
 
 // NODE is GNDStk::Node, which is an "incomplete type" to the compiler here.
 template<class NODE>
-bool json2node(const std::string &key, const orderedJSON &value, NODE &node)
+bool json2node(const std::string &key, const orderedJSON &siblings, NODE &node)
 {
-   // the node sent here should be fresh, ready to receive entries
+   // The node sent here should be fresh, ready to receive entries
    if (!node.empty())
-      json2node_error("!node.empty()");
+      json2node_error("Node not empty()");
 
-   // non-(JSON "objects") should have been handled in the caller
-   if (!value.is_object())
-      json2node_error("JSON value !is_object()");
+   // Non-(JSON "objects") should have been handled in the caller
+   if (!siblings.is_object())
+      json2node_error("JSON value !is_object() not expected here");
 
-   // the following cases should have been handled in the caller
+   // The following cases should have been handled in the caller
    if (endsin(key, "#nodename"))
-      json2node_error("JSON key ends in \"#nodename\"");
+      json2node_error("JSON key ending in \"#nodename\" not expected here");
    if (endsin(key, "#metadata"))
-      json2node_error("JSON key ends in \"#metadata\"");
+      json2node_error("JSON key ending in \"#metadata\" not expected here");
 
-   // node name: from key
+   // Node name: from key
    node.name = key;
 
    // ------------------------
@@ -136,52 +132,44 @@ bool json2node(const std::string &key, const orderedJSON &value, NODE &node)
    // ==> children
    // ------------------------
 
-   const orderedJSON &siblings = value;
-   for (auto keyval = value.begin();  keyval != value.end();  ++keyval) {
-      const std::string &key   = keyval.key();
-      const orderedJSON &child = keyval.value();
+   for (const auto &child : siblings.items()) {
+      const std::string &childkey = child.key();
+      const orderedJSON &childval = child.value();
 
-      if (key == "#nodename") {
+      if (childkey == "#nodename") {
          // *** #nodename
-         // Special entry, giving the parent Node's original name
-         node.name = child.get<std::string>();
-      } else if (key == "#metadata") {
+         node.name = childval.get<std::string>();
+      } else if (childkey == "#metadata") {
          // *** #metadata
-         for (auto attr = child.begin();  attr != child.end();  ++attr)
-            json_pair(node, orderedJSON{}, attr.key(), *attr, true);
-      } else if (beginsin(key,"#cdata") || beginsin(key,"#comment")) {
-         // *** #cdata[N]/#comment[N]
-         // Expand into a child node #cdata/#comment with a #text attribute.
-         if (child.is_object())
+         for (const auto &m : childval.items())
+            json_pair(node, orderedJSON{}, m.key(), m.value(), true);
+      } else if (
+         beginsin(childkey,"#cdata") ||
+         beginsin(childkey,"#comment") ||
+         beginsin(childkey,"#pcdata")
+      ) {
+         // *** #cdata[N], #comment[N], #pcdata[N]
+         if (childval.is_object())
             try {
-               if (!json2node(key, child, node.add()))
+               if (!json2node(childkey, childval, node.add()))
                   return false;
             } catch (...) {
                log::function("json2node()");
                throw;
             }
          else
-            node.add(beginsin(key,"#cdata") ? "#cdata" : "#comment")
-                .add("#text",child.get<std::string>());
-      } else if (beginsin(key,"#pcdata")) {
-         // *** #pcdata[N]
-         if (child.is_object())
-            try {
-               if (!json2node(key, child, node.add()))
-                  return false;
-            } catch (...) {
-               log::function("json2node()");
-               throw;
-            }
-         else // array
-            node.add("#pcdata").add("#text", json_array(child));
-      } else if (endsin(key,"#nodename") || endsin(key,"#metadata")) {
+            beginsin(childkey,"#pcdata")
+          ? node.add("#pcdata" ).add("#text", json_array(childval))
+          : beginsin(childkey,"#cdata")
+          ? node.add("#cdata"  ).add("#text", childval.get<std::string>())
+          : node.add("#comment").add("#text", childval.get<std::string>());
+      } else if (endsin(childkey,"#nodename") || endsin(childkey,"#metadata")) {
          // *** Ignore, in this context
       } else {
          // *** General case
-         json_pair(node, siblings, key, child, false);
+         json_pair(node, siblings, childkey, childval, false);
       }
-   } // for each element of the key/value pair corresponding to the Node
+   }
 
    // done
    return true;
