a996ae3ffa6110cc17f94cdffd35ac949635c1c4
Martin Staley <staley@lanl.gov>
2022-07-14 21:45:59 -0600

    Added comment.

--- c/src/GNDStk.h
+++ c/src/GNDStk.h
@@ -26,6 +26,15 @@
 extern_C
 void color(const int);
 
+// todo
+// Obviously, we'll want much, much more here. I'll probably add additional
+// C-style interfaces to GNDStk's C++ code gradually, as we discover what
+// our C language users want. Note that we're speaking here about the C
+// interface to the overarching GNDStk library, not a C interface to some
+// particular set of generated C++ classes that somebody might have created
+// with our code generator. Those C interfaces are (or will be) created
+// automatically, by the code generator, just as the C++ classes are.
+
 
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
