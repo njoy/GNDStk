e983b3954606bcbe5715c8b213806ef7aae64e23
Martin Staley <staley@lanl.gov>
2023-02-03 02:07:20 -0700

    Slight simplifications in the example code.

--- standards/gnds-2.0-alpha/example.cpp
+++ standards/gnds-2.0-alpha/example.cpp
@@ -10,20 +10,19 @@ void read(const Node &node)
 {
    T obj;
    obj.read(node);
-   //obj.print();
+   // obj.print();
 }
 
 // main
 int main(const int argc, const char *const *const argv)
 {
-   /*
-   colors = true;
-   shades = true;
-   indent = 3;
-   */
    colors = false;
    shades = false;
    indent = 1;
+   // Consider these instead if you use print() above:
+   // colors = true;
+   // shades = true;
+   // indent = 3;
 
    for (int i = 1; i < argc; ++i) {
       const std::string file = argv[i];
@@ -41,50 +40,43 @@ int main(const int argc, const char *const *const argv)
          // examine the top-level node in order to determine what kind of object
          // we're dealing with.
 
-         if (beginsin(file,"files/a-"))
-            read<common::ReactionSuite>(node);
-         else if (beginsin(file,"files/atom-"))
-            read<sfy::PoPs>(node);
-         else if (beginsin(file,"files/d-"))
-            read<common::ReactionSuite>(node);
-         else if (beginsin(file,"files/dec-"))
-            read<sfy::PoPs>(node);
-         else if (beginsin(file,"files/e-"))
-            read<common::ReactionSuite>(node);
-         else if (beginsin(file,"files/g-"))
-            read<common::ReactionSuite>(node);
-         else if (beginsin(file,"files/h-"))
-            read<common::ReactionSuite>(node);
-         else if (beginsin(file,"files/n-")) {
-            if (node.name == "reactionSuite")
-               read<common::ReactionSuite>(node);
-            else
-               read<covariance::CovarianceSuite>(node);
+         if (beginsin(file,"files/a-") ||
+             beginsin(file,"files/d-") ||
+             beginsin(file,"files/e-") ||
+             beginsin(file,"files/g-") ||
+             beginsin(file,"files/h-") ||
+             beginsin(file,"files/p-") ||
+             beginsin(file,"files/photoat-") ||
+             beginsin(file,"files/t-") ||
+             beginsin(file,"files/tsl-")) {
+            // is ReactionSuite
+            read<top::ReactionSuite>(node);
+         } else if (beginsin(file,"files/atom-") ||
+                    beginsin(file,"files/dec-") ||
+                    beginsin(file,"files/sfy-")) {
+            // is PoPs
+            read<top::PoPs>(node);
+         } else if (beginsin(file,"files/nfy-")) {
+            // is FissionFragmentData
+            read<top::FissionFragmentData>(node);
+         } else if (beginsin(file,"files/n-")) {
+            // can be either of two things...
+            node.name == "reactionSuite"
+          ? read<top::ReactionSuite>(node)
+          : read<top::CovarianceSuite>(node);
          }
-         else if (beginsin(file,"files/nfy-"))
-            read<common::FissionFragmentData>(node);
-         else if (beginsin(file,"files/p-"))
-            read<common::ReactionSuite>(node);
-         else if (beginsin(file,"files/photoat-"))
-            read<common::ReactionSuite>(node);
-         else if (beginsin(file,"files/sfy-"))
-            read<sfy::PoPs>(node);
          else if (beginsin(file,"files/std-")) {
-            if (node.name == "reactionSuite")
-               read<common::ReactionSuite>(node);
-            else if (node.name == "covarianceSuite")
-               read<covariance::CovarianceSuite>(node);
-            else if (node.name == "PoPs")
-               read<sfy::PoPs>(node);
-            else
-               assert(false);
+            // can be any of three things...
+            node.name == "reactionSuite"
+          ? read<top::ReactionSuite>(node)
+          : node.name == "covarianceSuite"
+          ? read<top::CovarianceSuite>(node)
+          : read<top::PoPs>(node);
          }
-         else if (beginsin(file,"files/t-"))
-            read<common::ReactionSuite>(node);
-         else if (beginsin(file,"files/tsl-"))
-            read<common::ReactionSuite>(node);
-         else
+         else {
+            // oops, should have done *something* above
             assert(false);
+         }
 
       } catch (...) {
          if (i < argc-1)
