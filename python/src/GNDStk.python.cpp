2d2d3e61da95920df78a211424da55b5d8375790
Martin Staley <staley@lanl.gov>
2023-02-07 17:40:32 -0700

    With luck, Python tests up and running again. (Without luck, Python tests not up and running again.)

--- python/src/GNDStk.python.cpp
+++ python/src/GNDStk.python.cpp
@@ -1,41 +1,32 @@
+
 // system includes
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-
-// other includes
 
 // namespace aliases
 namespace python = pybind11;
 
 // core interface declarations
 namespace core {
+   void wrapNode( python::module& );
 
-  void wrapNode( python::module& );
-
-  void wrapContributorType( python::module& );
-  void wrapDateType( python::module& );
-  void wrapDecayType( python::module& );
-  void wrapFrame( python::module& );
-  void wrapGridStyle( python::module& );
-  void wrapHashAlgorithm( python::module& );
-  void wrapInteraction( python::module& );
-  void wrapInterpolation( python::module& );
-  void wrapInterpolationQualifier( python::module& );
-  void wrapParity( python::module& );
-  void wrapRelationType( python::module& );
-  void wrapBoundaryCondition( python::module& );
-}
-
-// v1.9 interface declarations
-namespace python_v1_9 {
-
-  void wrapGNDS( python::module& );
+   void wrapContributorType( python::module& );
+   void wrapDateType( python::module& );
+   void wrapDecayType( python::module& );
+   void wrapFrame( python::module& );
+   void wrapGridStyle( python::module& );
+   void wrapHashAlgorithm( python::module& );
+   void wrapInteraction( python::module& );
+   void wrapInterpolation( python::module& );
+   void wrapInterpolationQualifier( python::module& );
+   void wrapParity( python::module& );
+   void wrapRelationType( python::module& );
+   void wrapBoundaryCondition( python::module& );
 }
 
 // v2.0 interface declarations
 namespace python_v2_0 {
-
-  void wrapGNDS( python::module& );
+   void wrapGNDS( python::module& );
 }
 
 /**
@@ -45,34 +36,29 @@ namespace python_v2_0 {
  *  set on the PROPERTIES OUTPUT_NAME in the CMakeLists.txt file.
  */
 PYBIND11_MODULE( GNDStk, module ) {
+   // create the core submodule
+   python::module submodule = module.def_submodule(
+      "core",
+      "core - GNDS core interface components"
+   );
 
-  // create the core submodule
-  python::module submodule = module.def_submodule(
+   // wrap core components (in the core module)
+   core::wrapNode( submodule );
 
-    "core",
-    "core - GNDS core interface components"
-  );
+   // enumerations (in the GNDStk module)
+   core::wrapContributorType( module );
+   core::wrapDateType( module );
+   core::wrapDecayType( module );
+   core::wrapFrame( module );
+   core::wrapGridStyle( module );
+   core::wrapHashAlgorithm( module );
+   core::wrapInteraction( module );
+   core::wrapInterpolation( module );
+   core::wrapInterpolationQualifier( module );
+   core::wrapParity( module );
+   core::wrapRelationType( module );
+   core::wrapBoundaryCondition( module );
 
-  // wrap core components (in the core module)
-  core::wrapNode( submodule );
-
-  // enumerations (in the GNDStk module)
-  core::wrapContributorType( module );
-  core::wrapDateType( module );
-  core::wrapDecayType( module );
-  core::wrapFrame( module );
-  core::wrapGridStyle( module );
-  core::wrapHashAlgorithm( module );
-  core::wrapInteraction( module );
-  core::wrapInterpolation( module );
-  core::wrapInterpolationQualifier( module );
-  core::wrapParity( module );
-  core::wrapRelationType( module );
-  core::wrapBoundaryCondition( module );
-
-  // v1.9 components (in the v1_9 module, created in this function)
-  python_v1_9::wrapGNDS( module );
-
-  // v2.0 components (in the v2_0 module, created in this function)
-  python_v2_0::wrapGNDS( module );
+   // v2.0 components (in the v2_0 module, created in this function)
+   python_v2_0::wrapGNDS( module );
 }
--- python/src/v2.0/GNDS.python.cpp
+++ python/src/v2.0/GNDS.python.cpp
@@ -1,37 +1,29 @@
+
 // system includes
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-
-// other includes
 
 // namespace aliases
 namespace python = pybind11;
 
 // v2.0 interface declarations
 namespace python_v2_0 {
+   void wrapG2d( python::module& );
+   void wrapG3d( python::module& );
+   void wrapGeneral( python::module& );
+   void wrapReduced( python::module& );
+   void wrapTop( python::module& );
 
-  void wrapContainers( python::module& );
-  void wrapFpy( python::module& );
-  void wrapFissionFragmentData( python::module& );
-  void wrapPops( python::module& );
-  void wrapResonances( python::module& );
-}
-
-namespace python_v2_0 {
-
-  void wrapGNDS( python::module& module ) {
-
-    // create the core submodule
-    python::module submodule = module.def_submodule(
-
-      "v2_0",
-      "GNDS 2.0 standard components"
-    );
-
-    python_v2_0::wrapContainers( submodule );
-    python_v2_0::wrapFpy( submodule );
-    python_v2_0::wrapFissionFragmentData( submodule );
-    python_v2_0::wrapPops( submodule );
-    python_v2_0::wrapResonances( submodule );
-  }
+   void wrapGNDS( python::module& module ) {
+      // create the core submodule
+      python::module submodule = module.def_submodule(
+         "v2_0",
+         "GNDS 2.0 standard components"
+      );
+      python_v2_0::wrapG2d( submodule );
+      python_v2_0::wrapG3d( submodule );
+      python_v2_0::wrapGeneral( submodule );
+      python_v2_0::wrapReduced( submodule );
+      python_v2_0::wrapTop( submodule );
+   }
 } // v2_0 namespace
