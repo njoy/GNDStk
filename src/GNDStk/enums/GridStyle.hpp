4d6e5c2b9cf95db4b8193932db21debc20e8a7da
Martin Staley <staley@lanl.gov>
2023-07-26 13:15:32 -0600

    Some reworking of certain enumerators.
    Part of a broader, moderate reorganization of parts of GNDStk.
    
    Some changes were just stylistic, to be consistent with other parts of the code base.
    
    Also made some basic simplifications and shortening. For example, eliminating some unnecessary namespace prefixes.

--- src/GNDStk/enums/GridStyle.hpp
+++ src/GNDStk/enums/GridStyle.hpp
@@ -1,60 +1,55 @@
-#ifndef NJOY_GNDSTK_GRIDSTYLE
-#define NJOY_GNDSTK_GRIDSTYLE
+
+#ifndef NJOY_GNDSTK_ENUMS_GRIDSTYLE
+#define NJOY_GNDSTK_ENUMS_GRIDSTYLE
 
 namespace enums {
 
-  /**
-   *  @class
-   *  @brief Enumeration class giving acceptable grid style values
-   */
-  enum class GridStyle {
+   /**
+    *  @class
+    *  @brief Enumeration class giving acceptable grid style values
+    */
+   enum class GridStyle {
+      none,
+      link,
+      points,
+      boundaries,
+      parameters
+   };
 
-    none,
-    link,
-    points,
-    boundaries,
-    parameters
-  };
+   /**
+    *  @brief Return whether or not a string is a valid grid style
+    *
+    *  @param[in] string    the string to be verified
+    *
+    *  @return true/false
+    */
+   inline bool isGridStyle(const std::string &string)
+   {
+      return isSymbol<GridStyle>(string);
+   }
 
-  /**
-   *  @brief Return whether or not a string is a valid grid style
-   *
-   *  @param[in] string    the string to be verified
-   *
-   *  @return true/false
-   */
-  inline bool isGridStyle( const std::string& string ) {
+   /**
+    *  @brief Template specialisation to convert GridStyle to/from strings
+    */
+   template<>
+   struct Map<GridStyle>
+   {
+      static inline const std::map<GridStyle,std::string> values {
+         { GridStyle::none, "none" },
+         { GridStyle::link, "link" },
+         { GridStyle::points, "points" },
+         { GridStyle::boundaries, "boundaries" },
+         { GridStyle::parameters, "parameters" }
+      };
 
-    return isSymbol< GridStyle >( string );
-  }
-
-  /**
-   *  @brief Template specialisation to convert GridStyle to/from strings
-   */
-  template <>
-  class Map< GNDStk::enums::GridStyle > {
-  public:
-
-    static inline const std::map< GNDStk::enums::GridStyle,
-                                  const std::string > values{
-
-      { GNDStk::enums::GridStyle::none,       "none" },
-      { GNDStk::enums::GridStyle::link,       "link" },
-      { GNDStk::enums::GridStyle::points,     "points" },
-      { GNDStk::enums::GridStyle::boundaries, "boundaries" },
-      { GNDStk::enums::GridStyle::parameters, "parameters" }
-    };
-
-    static inline const std::map< const std::string,
-                                  GNDStk::enums::GridStyle > symbols{
-
-      { "none", GNDStk::enums::GridStyle::none },
-      { "link", GNDStk::enums::GridStyle::link },
-      { "points", GNDStk::enums::GridStyle::points },
-      { "boundaries", GNDStk::enums::GridStyle::boundaries },
-      { "parameters", GNDStk::enums::GridStyle::parameters }
-    };
-  };
+      static inline const std::map<std::string,GridStyle> symbols {
+         { "none", GridStyle::none },
+         { "link", GridStyle::link },
+         { "points", GridStyle::points },
+         { "boundaries", GridStyle::boundaries },
+         { "parameters", GridStyle::parameters }
+      };
+   };
 
 } // namespace enums
 
--- src/GNDStk/enums/StorageOrder.hpp
+++ src/GNDStk/enums/StorageOrder.hpp
@@ -1,54 +1,48 @@
 
-#ifndef NJOY_GNDSTK_STORAGEORDER
-#define NJOY_GNDSTK_STORAGEORDER
+#ifndef NJOY_GNDSTK_ENUMS_STORAGEORDER
+#define NJOY_GNDSTK_ENUMS_STORAGEORDER
 
 namespace enums {
 
-  /**
-   *  @class
-   *  @brief Enumeration class giving acceptable storage order values
-   *
-   *  See GNDS v1.9 specifications section 3.5.3
-   */
-  enum class StorageOrder {
+   /**
+    *  @class
+    *  @brief Enumeration class giving acceptable storage order values
+    *
+    *  See GNDS v1.9 specifications section 3.5.3
+    */
+   enum class StorageOrder {
+      rowMajor = 1,
+      columnMajor = 2
+   };
 
-    rowMajor = 1,
-    columnMajor = 2
-  };
+   /**
+    *  @brief Return whether or not a string is a valid storage order
+    *
+    *  @param[in] string    the string to be verified
+    *
+    *  @return true/false
+    */
+   inline bool isStorageOrder(const std::string &string)
+   {
+      return isSymbol<StorageOrder>(string);
+   }
 
-  /**
-   *  @brief Return whether or not a string is a valid storage order
-   *
-   *  @param[in] string    the string to be verified
-   *
-   *  @return true/false
-   */
-  inline bool isStorageOrder( const std::string& string ) {
+   /**
+    *  @brief Template specialisation to convert StorageOrder to/from strings
+    */
+   template<>
+   struct Map<StorageOrder>
+   {
+      static inline const std::map<StorageOrder,std::string> values {
+         { StorageOrder::rowMajor, "rowMajor" },
+         { StorageOrder::columnMajor, "columnMajor" }
+      };
 
-    return isSymbol< StorageOrder >( string );
-  }
-
-  /**
-   *  @brief Template specialisation to convert StorageOrder to/from strings
-   */
-  template <>
-  class Map< GNDStk::enums::StorageOrder > {
-  public:
-
-    static inline const std::map< GNDStk::enums::StorageOrder,
-                                  const std::string > values{
-
-      { GNDStk::enums::StorageOrder::rowMajor, "rowMajor" },
-      { GNDStk::enums::StorageOrder::columnMajor, "columnMajor" }
-    };
-
-    static inline const std::map< const std::string,
-                                  GNDStk::enums::StorageOrder > symbols{
-
-      { "rowMajor", GNDStk::enums::StorageOrder::rowMajor },
-      { "columnMajor", GNDStk::enums::StorageOrder::columnMajor }
-    };
-  };
+      static inline const std::map<std::string,StorageOrder> symbols {
+         { "rowMajor", StorageOrder::rowMajor },
+         { "columnMajor", StorageOrder::columnMajor }
+      };
+   };
 
 } // namespace enums
 
